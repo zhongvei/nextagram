a4e2432bd26ce290b569f05618fef86aee6a95c5
zhongvei <zhongvei@gmail.com>
2020-02-03 11:55:49 +0800

    commit on 'master'

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { Container, Row, Col, Image } from 'react-bootstrap';
 import './App.css';
 
+
+
+
 function App() {
+
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    axios.get('https://insta.nextacademy.com/api/v1/users')
+      .then(result => {
+        setUsers([...result.data]);
+        console.log(users)
+      })
+      .catch(error => {
+        console.log('ERROR: ', error)
+      })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    < Container fluid >
+      {users.map(user => (
+        <Row>
+          <Col sm={12} key={user.id}>
+            <Image src={user.profileImage} roundedCircle style={{ width: "20%" }} />
+          </Col>
+        </Row>
+      ))}
+    </Container >
+
   );
-}
+};
 
 export default App;
